79daed63f00b5160c8247603e7ea911fc0737602
Abhishek gupta <agupta4@albany.edu>
2016-11-11 02:10:05 -0500

    Second Commit

--- OnlineGraphIHT/src/main/java/edu/cs/albany/Functions/MeanScanStat.java
+++ OnlineGraphIHT/src/main/java/edu/cs/albany/Functions/MeanScanStat.java
@@ -1,6 +1,18 @@
 package edu.cs.albany.Functions;
 
-//Implementation of -W^Tx/1^Tx
-public class MeanScanStat {
+import edu.cs.albany.Interface.Function;
+
+//Implementation of mean scan statistics -Ws^Tx + (Wcap)s^Tx/1^Tx
+public class MeanScanStat implements Function{
+
+	public double Func_Value() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double Gradient() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
---
+++ OnlineGraphIHT/src/main/java/edu/cs/albany/Interface/Function.java
@@ -0,0 +1,8 @@
+package edu.cs.albany.Interface;
+//Needed to be implemented by every function
+public interface Function {
+	
+	public double Func_Value();
+	public double Gradient();
+	
+}
