a582e9cdd5f9f126b110f5e0fff5b4c08e612c68
Abhishek gupta <agupta4@albany.edu>
2016-11-11 01:58:00 -0500

    First commit

---
+++ OnlineGraphIHT/src/main/java/edu/cs/albany/Functions/MeanScanStat.java
@@ -0,0 +1,6 @@
+package edu.cs.albany.Functions;
+
+//Implementation of -W^Tx/1^Tx
+public class MeanScanStat {
+
+}
---
+++ OnlineGraphIHT/src/main/java/edu/cs/albany/Main/IHT.java
@@ -0,0 +1,10 @@
+package edu.cs.albany.Main;
+
+public class IHT {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
